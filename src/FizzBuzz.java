0d5a48a4066fbdf091c50378b2bd46e1875d80d2
Vlad Grigore <vladyg1986@gmail.com>
2021-02-01 12:25:50 +0200

    second commit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,6 +2,7 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         System.out.println("This is my program!");
+        System.out.println("This is an update!");
 
     }
 }
